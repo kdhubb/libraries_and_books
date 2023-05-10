4dc95abc888e3c9d4b1f65bf034888fed0f924f0
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-10 17:21:28 -0600

    edit books show test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    require 'pry'; binding.pry
+    @book = Book.find(params[:id])
+  end
 end
--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -18,18 +18,16 @@ RSpec.describe "books show page", type: :feature do
                                 branch_name: "Athmar Park",
                                 has_study_rooms: true,
                                 num_public_computers: 15)
-    book_1 = Book.create!(barcode: 8374,
+    book_1 = library_1.books.create!(barcode: 8374,
                           author: "Andy Weir",
                           title: "Project Hail Mary",
                           on_shelf: false,
-                          ytd_circ: 10,
-                          library_id: library_1.id)
-    book_2 = Book.create!(barcode: 9274,
+                          ytd_circ: 10)
+    book_2 = library_2.books.create!(barcode: 9274,
                           author: "Louise Erdrich",
                           title: "The Night Watchman",
                           on_shelf: true,
-                          ytd_circ: 3,
-                          library_id: library_2.id)
+                          ytd_circ: 3)
 
     visit "/books/#{book_1.id}"
     save_and_open_page

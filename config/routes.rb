1fe6915f931fbde957eef4bb34dfaf961819ab01
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-11 16:52:24 -0600

    add create edit destroy routes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   get "/books", to: "books#index"
   get "/books/:id", to: "books#show"
   get "/libraries/:id/books", to: "library_books#index"
+  get "/libraries/new", to: "libraries#new"
+  post "/libraries", to: "libraries#create"
+  get "/libraries/:id/edit", to: "libraries#edit"
+  patch "/libraries", to: "libraries#update"
+  delete "/libraries/:id", to: "libraries#destroy" 
 end

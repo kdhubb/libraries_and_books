dd9a667432c8d79312a44dfc49fa7a0b55d8e82f
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-12 23:21:32 -0600

    TODO: Sort query link is working but sort method is not working with sort query link. When "Sort Books by Author" link is clicked in library_books index page, the page is directed to link address, but no sorting happens.

--- spec/features/libraries/books/index_spec.rb
+++ spec/features/libraries/books/index_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe "A library's books index page", type: :feature do
                                 branch_name: "Athmar Park",
                                 has_study_rooms: true,
                                 num_public_computers: 15)
-    book_1 = library_1.books.create!(barcode: 8374,
-                                      author: "Andy Weir",
-                                      title: "Project Hail Mary",
-                                      on_shelf: false,
-                                      ytd_circ: 10)
+    book_1 = library_1.books.create!(barcode: 8353,
+                                      author: "Octavia Butler",
+                                      title: "Dawn",
+                                      on_shelf: true,
+                                      ytd_circ: 3)
     book_2 = library_2.books.create!(barcode: 9274,
                                       author: "Louise Erdrich",
                                       title: "The Night Watchman",
                                       on_shelf: true,
                                       ytd_circ: 3)
-    book_3 = library_1.books.create!(barcode: 8353,
-                                      author: "Octavia Butler",
-                                      title: "Dawn",
-                                      on_shelf: true,
-                                      ytd_circ: 3)
+    book_3 = library_1.books.create!(barcode: 8374,
+                                      author: "Andy Weir",
+                                      title: "Project Hail Mary",
+                                      on_shelf: false,
+                                      ytd_circ: 10)
     book_4 = library_2.books.create!(barcode: 9221,
                                       author: "Viet Thanh Nguyen",
                                       title: "The Sympathizer",

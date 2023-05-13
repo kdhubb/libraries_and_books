2bea17d25c323796ff8f1cf9cf1f6693ef7d4fa6
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-12 22:10:37 -0600

    improve sort by author tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Book, type: :model do
                                   branch_name: "Athmar Park",
                                   has_study_rooms: true,
                                   num_public_computers: 15)
-    @book_1 = @library_1.books.create!(barcode: 8374,
+    @book_5 = @library_1.books.create!(barcode: 8374,
                                         author: "Andy Weir",
                                         title: "Project Hail Mary",
                                         on_shelf: false,
@@ -33,16 +33,22 @@ RSpec.describe Book, type: :model do
                                         title: "The Sympathizer",
                                         on_shelf: true,
                                         ytd_circ: 2)
+    @book_1 = @library_2.books.create!(barcode: 2987,
+                                        author: "Kali Fajardo-Anstine",
+                                        title: "Sabrina and Corina",
+                                        on_shelf: true,
+                                        ytd_circ: 5)
+    
   end
   describe "class methods" do 
     it "show_on_shelf" do 
       # returns all books where on_shelf is true
-      expect(Book.show_on_shelf).to eq([@book_2, @book_4])
+      expect(Book.show_on_shelf).to eq([@book_2, @book_4, @book_1])
     end
 
     it "sort_by_author" do 
       # returns all books in alphabetical order by author
-      expect(Book.sort_by_author).to eq([@book_1, @book_2, @book_3, @book_4])
+      expect(Book.sort_by_author).to eq([@book_5, @book_1, @book_2, @book_3, @book_4])
     end
   end
 end

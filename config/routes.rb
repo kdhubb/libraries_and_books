48cce7076692e5af1ed4411f32fcbab1de6f4cda
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-15 18:51:05 -0600

    finish formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get "/libraries", to: "libraries#index"
+  post "/libraries", to: "libraries#create"
   get "/libraries/new", to: "libraries#new"
   get "/libraries/:id", to: "libraries#show"
+  get "/libraries/:id/edit", to: "libraries#edit"
+  patch "/libraries/:id", to: "libraries#update"
+  delete "/libraries/:id", to: "libraries#destroy" 
+
   get "/books", to: "books#index"
   get "/books/:id", to: "books#show"
   get "/books/:id/edit", to: "books#edit"
   patch "/books/:id", to: "books#update"
+  delete "/books/:id", to: "books#destroy" 
+
   get "/libraries/:id/books", to: "library_books#index"
-  post "/libraries", to: "libraries#create"
-  get "/libraries/:id/edit", to: "libraries#edit"
-  patch "/libraries/:id", to: "libraries#update"
   get "/libraries/:id/books/new", to: "library_books#new"
   post "/libraries/:id/books", to: "library_books#create"
-  delete "/libraries/:id", to: "libraries#destroy" 
-  delete "/books/:id", to: "books#destroy" 
 end
